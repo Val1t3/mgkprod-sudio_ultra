9509002bbd7aa37529647d4583bf74e64e72aabc
Val <valentinwoehrel2003@gmail.com>
2022-12-19 09:36:48 +0100

    [ADD] header with video

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,34 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        gray: {
+          500: "#838386"
+        },
+        black: {
+          300: "#1b191e",
+          500: "#121217"
+        },
+        orange: {
+          500: "#ea3d25"
+        }
+      },
+      borderWidth: {
+        '1': '1px',
+      },
+      spacing: {
+        '436': '436.5px'
+      },
+      fontSize: {
+        lg: '22px',
+        xl: '28px',
+        '2xl': '56px',
+        '3xl': '80px',
+        '4xl': '128px'
+      },
+      letterSpacing: {
+        tight: '-2px',
+      }
     },
   },
   plugins: [],
