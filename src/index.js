909588e4a36361542aadacd11b3895d46ed7f2c6
Val <valentinwoehrel2003@gmail.com>
2022-12-22 02:07:41 +0100

    [ADD] js choice btn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
 window.addEventListener("scroll", () => {
     let header = document.querySelector("#header-fixed");
     header.classList.toggle("head-fixed", window.scrollY >= 250);
-})
+});
+
+let ready = true;
+
+document.querySelector("#ready-btn").addEventListener("click", () => {
+    ready = !ready;
+
+    if (ready) {
+        document.querySelector("#price-1").innerHTML = "$3K";
+        document.querySelector("#price-2").innerHTML = "$10K";
+        document.querySelector("#price-3").innerHTML = "$20K+";
+        document.querySelector("#ready-des-1").innerHTML = "Design, Frontend & Backend";
+        document.querySelector("#ready-des-2").innerHTML = "Design, Frontend & Backend";
+        document.querySelector("#ready-des-3").innerHTML = "Design, Frontend & Backend";
+    } else {
+        document.querySelector("#price-1").innerHTML = "$2K";
+        document.querySelector("#price-2").innerHTML = "$5K";
+        document.querySelector("#price-3").innerHTML = "$15K+";
+        document.querySelector("#ready-des-1").innerHTML = "Frontend & Backend";
+        document.querySelector("#ready-des-2").innerHTML = "Frontend & Backend";
+        document.querySelector("#ready-des-3").innerHTML = "Frontend & Backend";
+    }
+});
